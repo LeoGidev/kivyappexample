9c2a0e9fc3cd9717e0962c1e5b23a2dfb93a24ff
Leo Gimenez <leogimenez.dev@gmail.com>
2025-01-16 12:40:47 -0300

    se colocan librerías utiles

--- main.py
+++ main.py
@@ -5,3 +5,4 @@ from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
+from kivy.uix.gridlayout import GridLayout
