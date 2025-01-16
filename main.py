815560991626cd31fde3bbf30875c25a50ab1c0e
Leo Gimenez <leogimenez.dev@gmail.com>
2025-01-16 12:40:14 -0300

    se colocan librerías utiles

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.image import Image
+from kivy.uix.label import Label
