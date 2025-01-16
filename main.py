92d704964f84ceb485747a5f4026fe471bfeb1da
Leo Gimenez <leogimenez.dev@gmail.com>
2025-01-16 12:40:31 -0300

    se colocan librerías utiles

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.image import Image
 from kivy.uix.label import Label
+from kivy.uix.scrollview import ScrollView
